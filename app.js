3a4860b34c640ab9db618b84c620a202db037e39
DatBoiii <90166694+DatBoiii@users.noreply.github.com>
2021-10-19 10:50:16 -0400

    Update
    To-Do-List code has been added

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-ECHO is on.
+let addToDoButton = document.getElementById('addbutton');
+let ToDocontainer = document.getElementById('list');
+let inputField = document.getElementById('myinput')
+
+addToDoButton.addEventListener('click', function() {
+	let paragraph = document.createElement('li')
+	paragraph.innerText = myinput.value;
+	ToDocontainer.appendChild(paragraph);
+	myinput.value = "";
+	paragraph.addEventListener('click', function() {
+		ToDocontainer.removeChild(paragraph);
+	})
+})

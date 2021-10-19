7ec37064412eabebc97280d64e8a8b4438e3d94f
apulchritudoff <32424649+apulchritudoff@users.noreply.github.com>
2021-10-19 15:16:10 -0700

    updatedwith working version of Calculator

--- app-Calculator.js
+++ app-Calculator.js
@@ -1,27 +1,43 @@
-let slideshow = {
-    bill = 0,
-    numOfGuests = 0,
-    qualOfService: ['bad', 'okay', 'great'],
+let tipCalculator = {
+    bill: 0,
+    numOfGuests: 0,
+    qualOfService: 0,
+    extraTip: 0,
+    total: 0,
 
-    // takes billAmount and times it by .2
-    tip: function (amount) {
+    // takes function to grab input from user and calculate tip
 
-        extraTip = 0;
+    calculateTip: function() {
+        this.bill = document.getElementById("billInput").value;
+        this.qualOfService = document.getElementById("service").value;
+        this.numOfGuests = document.getElementById("guestInput").value;
 
-        if (numOfGuests > 4) {
-            extraTip = amount * .1;
+    // convert string data type to number data type
+
+       this.bill = parseInt(this.bill);
+       this.qualOfService = parseFloat(this.qualOfService);
+       this.numOfGuests = parseInt(this.numOfGuests);
+
+
+    // If there is a large group the minimum tip will be 18%
+
+        if (this.numOfGuests > 6 && this.qualOfService < .2) {
+            this.qualOfService = .18;
         }
+    
+    // get tip and total
 
-        return amount * 0.2;
+        this.extraTip = (this.bill * this.qualOfService) / this.numOfGuests;
+
+        this.total = this.bill + this.extraTip;
+
+
+    // set a variable to html ID and output to user
+
+       let output = document.getElementById("calOutput")
+
+       output.innerHTML = `The tip for your $${this.bill} bill would be
+       $${this.extraTip.toFixed(2)} for a total of $${this.total.toFixed(2)}`
     },
 
-    // returns bill and adds the tip
-    totalWithTip: function (amount) {
-        return tip(amount) + amount;
-    },
-
-    // function call to get total
-    getTotal: function{
-        console.log(`$${totalWithTip(this.bill).toFixed(2)}`);
-    }
 };

a929b777a3b6201150c045090d5b26f656854c60
apulchritudoff <32424649+apulchritudoff@users.noreply.github.com>
2021-10-20 07:17:52 -0700

    added a function to clear button

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ let clearToDoButton = document.getElementById('clearbutton');
 let ToDocontainer = document.getElementById('list');
 let inputField = document.getElementById('myinput')
 
-addToDoButton.addEventListener('click', function() {
-	let paragraph = document.createElement('li')
-	paragraph.innerText = myinput.value;
-	ToDocontainer.appendChild(paragraph);
-	myinput.value = "";
-	paragraph.addEventListener('click', function() {
-		paragraph.style.textDecoration = "line-through";
-	})
-    paragraph.addEventListener('dblclick', function() {
-		ToDocontainer.removeChild(paragraph);
-	})
+addToDoButton.addEventListener('click', function () {
+    let paragraph = document.createElement('li')
+    paragraph.innerText = myinput.value;
+    ToDocontainer.appendChild(paragraph);
+    myinput.value = "";
+    paragraph.addEventListener('click', function () {
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function () {
+        ToDocontainer.removeChild(paragraph);
+    })
+})
+
+clearToDoButton.addEventListener('click', function () {
+
+    let list = document.querySelector('#list');
+    console.log(length)
+    while (list.children[0] != null) {
+     //   console.log(i);
+        console.log(list.children[0]);
+        list.children[0].remove();
+    }
 })
